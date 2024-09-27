de18d3a9156a4e7824b86d4b982fd80adc54e03b
E-moL0587 <kai.pika.025@outlook.com>
2024-09-27 16:57:50 +0900

    update

--- server/src/main/java/com/example/server/VoxelController.java
+++ server/src/main/java/com/example/server/VoxelController.java
@@ -1,7 +1,7 @@
 package com.example.server;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VoxelController {
 
-    @Autowired
-    private MarchingCubes marchingCubes;
-
     @PostMapping("/upload")
     public List<VoxelCoordinates> uploadVoxel(@RequestBody List<VoxelCoordinates> coordinates) {
-        List<VoxelCoordinates> meshData = marchingCubes.generateMesh(coordinates);
-        return meshData;
+        return coordinates;
     }
 }
